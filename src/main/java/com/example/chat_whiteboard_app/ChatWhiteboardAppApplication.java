9106ee4d2b1c4c8776ee4c48e226f521d3c2eedb
joshuahT <joshuahtangs@gmail.com>
2024-11-30 16:07:29 -0500

    first commit

---
+++ src/main/java/com/example/chat_whiteboard_app/ChatWhiteboardAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.chat_whiteboard_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatWhiteboardAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatWhiteboardAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chat_whiteboard_app/ChatWhiteboardAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chat_whiteboard_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatWhiteboardAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
